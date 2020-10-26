a05aace8b1054a707f8a1c70595df3f1d724a771
= <=>
2020-10-27 01:31:45 +0530

    Export all components from a single entrypoint

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Button } from "./Button/Button";
+export { Dropdown } from "./Dropdown/Dropdown";
+export { TextInput } from "./TextInput/TextInput";
+export { Toast } from "./Toast/Toast";
+export { Timer } from "./Timer/Timer";
