7539c6a7e1c6577b0a385c1a916fb2e02e35d25f
= <=>
2020-10-27 16:31:43 +0530

    Add route for game end screen

--- src/RouteConfiguration.js
+++ src/RouteConfiguration.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, HashRouter } from "react-router-dom";
-import { StartScreen, GameScreen } from "./screens";
+import { StartScreen, GameScreen, EndScreen } from "./screens";
 
 const RouteConfiguration = () => {
   return (
     <HashRouter>
       <Route exact={true} path={"/"} component={StartScreen} />
       <Route exact={true} path={"/game"} component={GameScreen} />
+      <Route exact={true} path={"/results/:id"} component={EndScreen} />
     </HashRouter>
   );
 };
